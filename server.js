fd37167d3eab413537c01c147e802e707027a587
sali73 <sally.elgendy@hotmail.com>
2021-05-07 23:35:21 -0400

    change state port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const calculatorController = require("./controllers/calculator.js");
 const db = mongoose.connection;
